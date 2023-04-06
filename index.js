cef3b8f7b51c485ba83e88cbb6c95e247b376ef2
JeanLucMakiola <makiolajeanluc@gmail.com>
2023-04-06 16:10:07 +0200

    contact page half done

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const detectionbox = document.querySelector('.line-toggle-detectionbox');
 const navExpand = document.querySelector('.nav-expand-container');
 const backgroundblur = document.querySelector('.backgroundblur');
 
-console.log(backgroundblur);
+
 
 detectionbox.addEventListener('click', ()=>{
     detectionbox.classList.toggle('toggle-expanded');
     navExpand.classList.toggle('nav-expanded');
     backgroundblur.classList.toggle('blur-toggled');
+
 });
 
+var r = document.querySelector(':root');
 
+  
+  
+  function myFunction_set() {
+    r.style.setProperty('--test', '1');
+    
+  }
     
